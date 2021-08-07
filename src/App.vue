637aa425bd5a4f0ae58ced534776d1a821b1165d
kirito <chebot.kirill.r@gmail.com>
2021-08-07 18:35:17 +0300

    main page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
-<template>
-  <div id = "hello">
-    <H1>Hello, Sea Battle!</H1>
+<template id = "main">
+  <div id = "left">
+      <img src = "../public/ship.png" width = "250" height = "250">
+    <div id = "start_text">
+     <p>Морской Бой 2021</p>
+    </div>
+  </div>
+  <div id = "right">
+    <form><button id = "start">
+      <p>Начать</p>
+    </button></form>
+  
   </div>
 </template>
 <script>
 export default {
   name: "App",
+  data(){
+    return{
+      ticker:"default"
+    }
+  },
+  methods: {
+    add(){
+      alert("I am working!");
+    }
+  }
 };
 </script>
 <style src="./style.css"></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
+//import VueKonva from "vue-konva";
+//Vue.use(VueKonva);
 createApp(App).mount("#app");
